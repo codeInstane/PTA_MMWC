4594a8bdd1e2fc8100a69e7e6e0daf7c33305653
John Doe <johndoe@example.com>
2023-10-26 22:54:55 +0800

    log no. 002 password in 端午節親子包糉DIY工作坊

--- js/modal-login.js
+++ js/modal-login.js
@@ -1,50 +1,74 @@
 
-  // Get the modal element
-  var modal = document.getElementById("loginModal");
+// Close button functionality
+const closeButton = document.querySelector('.close-button');
+const videoContainer = document.querySelector('.video-container');
+const videoIframe = videoContainer.querySelector('iframe');
+const playButton = document.querySelector('.play-button');
 
-  // Get the close button
-  var closeBtn = document.getElementsByClassName("close")[0];
 
-  // Get the login form element
-  var loginForm = document.getElementById("loginForm");
+closeButton.addEventListener('click', () => {
+  const loginModal = document.getElementById('login-modal');
+  loginModal.style.display = 'none';
+  // Stop the video by removing the iframe src
+  videoIframe.src = '';
+  videoContainer.style.display = 'none';
+  playButton.style.display = 'block';
+});
 
-  // Add event listener to open the modal when the video link is clicked
-  document.querySelector(".popup-vimeo").addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent the default behavior of the link
 
-    // Show the modal
-    modal.style.display = "block";
-  });
-
-  // Add event listener to close the modal when the close button is clicked
-  closeBtn.addEventListener("click", function () {
-    modal.style.display = "none";
-  });
-
-  // Add event listener to close the modal when the user clicks outside the modal
-  window.addEventListener("click", function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  });
-
-  // Add event listener to handle the login form submission
-  loginForm.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the form from submitting
-
-    // Perform the login authentication here
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-
-    // Example authentication logic (replace with your own)
-    if (username === "admin" && password === "password") {
-      // Login successful, play the video
-      modal.style.display = "none"; // Hide the modal
-      var videoLink = document.querySelector(".popup-vimeo");
-      var videoUrl = videoLink.getAttribute("href");
-      window.location.href = videoUrl; // Redirect to the video URL
+// revised code on 2023-10-26
+  function showLoginModal() {
+    const modal = document.getElementById('login-modal');
+    modal.style.display = 'block';
+  }
+  
+  function authenticate(event) {
+    event.preventDefault();
+  
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    // Add your authentication logic here
+    const isAuthenticated = authenticateUser(username, password);
+    
+    if (isAuthenticated) {
+      showVideo();
     } else {
-      // Login failed, show the modal again
-      alert("Invalid username or password. Please try again.");
+      alert('Invalid username or password');
+      //console.log("Invalid username or password")
     }
-  });
+  }
+  
+  
+
+  function showVideo() {
+    const loginModal = document.getElementById('login-modal');
+    const videoContainer = document.querySelector('.video-container');    
+
+    playButton.style.display = 'none';
+    loginModal.style.display = 'none';
+    videoContainer.style.display = 'block';
+
+    document.getElementById('username').value = "";
+    document.getElementById('password').value = "";
+  }
+  
+
+  function closeLoginModal() {
+    const loginModal = document.getElementById('login-modal');
+    loginModal.style.display = 'none';
+    document.getElementById('username').value = "";
+    document.getElementById('password').value = "";
+  }
+
+  
+  function authenticateUser(username, password) {
+    // Add your authentication logic here
+    // Example: check against a hardcoded username and password
+    const validUsername = 'admin';
+    const validPassword = 'password';
+  
+    return username === validUsername && password === validPassword;
+  }
+
+  
